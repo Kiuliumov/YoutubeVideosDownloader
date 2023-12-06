3cdfbf3b6a55107dd3ae3f8799bc2bdbae016e7e
Kiuliumov <miti.bania@gmail.com>
2023-12-06 11:31:19 +0200

    Main Script

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import customtkinter
+from pytube import YouTube
+
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme('blue')
+app = customtkinter.CTk()
+app.geometry('720x480')
+app.title('Youtube to MP4')
+app.mainloop()
