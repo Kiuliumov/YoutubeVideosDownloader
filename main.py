0184f898a69f1f4697257fb2245d23eac38bab09
Kiuliumov <miti.bania@gmail.com>
2023-12-06 12:08:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
 app = customtkinter.CTk()
 app.geometry('356x224')
-app.title('Youtube Downloader')
+app.title('Youtube Downloader by Kiuliumov')
 
 title = customtkinter.CTkLabel(app, text='Insert a YouTube link!')
 title.pack(padx=10, pady=10)
